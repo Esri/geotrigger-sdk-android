557fa9f378b5c9586b06f9dec7109d3e809ef052
Court Fowler <courtf@gmail.com>
2013-11-13 17:11:12 -0800

    sample project and javadoc

---
+++ javadoc/reference/lists.js
@@ -0,0 +1,19 @@
+var DATA = [
+      { id:0, label:"com.esri.android.geotrigger", link:"com/esri/android/geotrigger/package-summary.html", type:"package" },
+      { id:1, label:"com.esri.android.geotrigger.GeotriggerApiClient", link:"com/esri/android/geotrigger/GeotriggerApiClient.html", type:"class" },
+      { id:2, label:"com.esri.android.geotrigger.GeotriggerApiListener", link:"com/esri/android/geotrigger/GeotriggerApiListener.html", type:"class" },
+      { id:3, label:"com.esri.android.geotrigger.GeotriggerBroadcastReceiver", link:"com/esri/android/geotrigger/GeotriggerBroadcastReceiver.html", type:"class" },
+      { id:4, label:"com.esri.android.geotrigger.GeotriggerBroadcastReceiver.DeviceReadyListener", link:"com/esri/android/geotrigger/GeotriggerBroadcastReceiver.DeviceReadyListener.html", type:"class" },
+      { id:5, label:"com.esri.android.geotrigger.GeotriggerBroadcastReceiver.DeviceRegisterFailListener", link:"com/esri/android/geotrigger/GeotriggerBroadcastReceiver.DeviceRegisterFailListener.html", type:"class" },
+      { id:6, label:"com.esri.android.geotrigger.GeotriggerBroadcastReceiver.LocationUpdateListener", link:"com/esri/android/geotrigger/GeotriggerBroadcastReceiver.LocationUpdateListener.html", type:"class" },
+      { id:7, label:"com.esri.android.geotrigger.GeotriggerBroadcastReceiver.LocationUploadFailListener", link:"com/esri/android/geotrigger/GeotriggerBroadcastReceiver.LocationUploadFailListener.html", type:"class" },
+      { id:8, label:"com.esri.android.geotrigger.GeotriggerBroadcastReceiver.LocationUploadListener", link:"com/esri/android/geotrigger/GeotriggerBroadcastReceiver.LocationUploadListener.html", type:"class" },
+      { id:9, label:"com.esri.android.geotrigger.GeotriggerBroadcastReceiver.PushMessageListener", link:"com/esri/android/geotrigger/GeotriggerBroadcastReceiver.PushMessageListener.html", type:"class" },
+      { id:10, label:"com.esri.android.geotrigger.GeotriggerBroadcastReceiver.PushTokenRegisterFailListener", link:"com/esri/android/geotrigger/GeotriggerBroadcastReceiver.PushTokenRegisterFailListener.html", type:"class" },
+      { id:11, label:"com.esri.android.geotrigger.GeotriggerBroadcastReceiver.PushTokenRegisterListener", link:"com/esri/android/geotrigger/GeotriggerBroadcastReceiver.PushTokenRegisterListener.html", type:"class" },
+      { id:12, label:"com.esri.android.geotrigger.GeotriggerBroadcastReceiver.TrackingProfileChangeListener", link:"com/esri/android/geotrigger/GeotriggerBroadcastReceiver.TrackingProfileChangeListener.html", type:"class" },
+      { id:13, label:"com.esri.android.geotrigger.GeotriggerService", link:"com/esri/android/geotrigger/GeotriggerService.html", type:"class" },
+      { id:14, label:"com.esri.android.geotrigger.MessageReceiver", link:"com/esri/android/geotrigger/MessageReceiver.html", type:"class" },
+      { id:15, label:"com.esri.android.geotrigger.TriggerBuilder", link:"com/esri/android/geotrigger/TriggerBuilder.html", type:"class" }
+
+    ];
